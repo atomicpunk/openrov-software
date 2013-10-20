150042dce593df1ca4a0e5bce2407d5cf0dd0c57
Todd Brandt <tebrandt@frontier.com>
2013-10-20 22:14:36 +0000

    changed update script to just rebuild and update the firmware

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 /etc/init.d/openrov stop
-git pull origin master
-/opt/openrov/updatelinux.sh
-/opt/node/bin/npm install
 /opt/openrov/linux/arduino/firmware-installfromsource.sh
 /etc/init.d/openrov start
