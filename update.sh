9a3eb906ab31133d3e7608ba158a52a5715be0e0
Todd Brandt <tebrandt@frontier.com>
2013-10-22 03:42:46 +0000

    added streamer script and an instance of the base firmware

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 /etc/init.d/openrov stop
+git pull origin master
+/opt/openrov/updatelinux.sh
+/opt/node/bin/npm install
 /opt/openrov/linux/arduino/firmware-installfromsource.sh
 /etc/init.d/openrov start
