dc5c18aeefc48678547f7456184218e8089a9377
Todd Brandt <tebrandt@frontier.com>
2013-11-05 03:31:01 +0000

    changed thrust inputs to allow combinations of keys

--- src/static/js/simple.js
+++ src/static/js/simple.js
@@ -1,39 +1,84 @@
 var Simple = function() {
 	var self = this;
 	this.socket = null;
-	this.keypressed = false;
+	this.VdT = 500;
+	this.HdT = 500;
 	this.cmdlist = {
-		81: { downcmd : ";tilt(1800);", upcmd : "" }, // q : tilt up
-		65: { downcmd : ";tilt(1350);", upcmd : "" }, // a : tilt middle
-		90: { downcmd : ";tilt(1000);", upcmd : "" }, // z : tilt down
-		87: { downcmd : ";light(255);", upcmd : "" }, // w : light full
-		83: { downcmd : ";light(127);", upcmd : "" }, // s : light half
-		88: { downcmd : ";light(0);", upcmd : "" },   // x : light off
-		38: { downcmd : ";go(2000,1500,2000);", upcmd : ";go(1500,1500,1500);" }, // up - fwd
-		40: { downcmd : ";go(1000,1500,1000);", upcmd : ";go(1500,1500,1500);" }, // down - bwd
-		37: { downcmd : ";go(1000,1500,2000);", upcmd : ";go(1500,1500,1500);" }, // left - left
-		39: { downcmd : ";go(2000,1500,1000);", upcmd : ";go(1500,1500,1500);" }, // right - right
-		33: { downcmd : ";go(1500,1000,1500);", upcmd : ";go(1500,1500,1500);" }, // pageup - up
-		34: { downcmd : ";go(1500,2000,1500);", upcmd : ";go(1500,1500,1500);" }, // pagedwn - dwn
+		81: { downcmd : ";tilt(1800);", pressed : 0 }, // q : tilt up
+		65: { downcmd : ";tilt(1350);", pressed : 0 }, // a : tilt middle
+		90: { downcmd : ";tilt(1000);", pressed : 0 }, // z : tilt down
+		87: { downcmd : ";light(255);", pressed : 0 }, // w : light full
+		83: { downcmd : ";light(127);", pressed : 0 }, // s : light half
+		88: { downcmd : ";light(0);", pressed : 0 },   // x : light off
+		38: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // up - fwd
+		40: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // down - bwd
+		37: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // left - left
+		39: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // right - right
+		33: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // pageup - up
+		34: { downcmd : "thrust", upcmd : "thrust", pressed : 0 }, // pagedwn - dwn
 	};
-	document.body.addEventListener("keydown", function(e) {
-		if(self.keypressed)
+	function thrust() {
+		var f = self.cmdlist[38].pressed;
+		var b = self.cmdlist[40].pressed;
+		var l = self.cmdlist[37].pressed;
+		var r = self.cmdlist[39].pressed;
+		var u = self.cmdlist[33].pressed;
+		var d = self.cmdlist[34].pressed;
+
+		// ignore the non-sensical inputs
+		if((l+r+f+b > 2)||(l && r)||(f && b)||(u && d))
 			return;
-		self.keypressed = true;
+
+		var vert = 1500 + (u?-self.VdT:0) + (d?self.VdT:0);
+		var star = 1500;
+		var port = 1500;
+		if(l && !r && !f && !b) {
+			port = 1000; star = 2000;
+		} else if(!l && r && !f && !b) {
+			port = 2000; star = 1000;
+		} else if(l && !r && f && !b) {
+			port = 1500; star = 2000;
+		} else if(!l && r && f && !b) {
+			port = 2000; star = 1500;
+		} else if(l && !r && !f && b) {
+			port = 1500; star = 1000;
+		} else if(!l && r && !f && b) {
+			port = 1000; star = 1500;
+		} else if(!l && !r && f && !b) {
+			port = 2000; star = 2000;
+		} else if(!l && !r && !f && b) {
+			port = 1000; star = 1000;
+		}
+
+		var cmd = ";go("+star+","+vert+","+port+");"
+		self.socket.emit('raw_command', cmd);
+	}
+	document.body.addEventListener("keydown", function(e) {
 		if(!(e.keyCode in self.cmdlist))
 			return;
+		if(self.cmdlist[e.keyCode].pressed)
+			return;
+		self.cmdlist[e.keyCode].pressed = 1;
 		var cmd = self.cmdlist[e.keyCode].downcmd;
-		self.socket.emit('raw_command', cmd);
+		if(!cmd)
+			return;
+		if(cmd[0] == ';')
+			self.socket.emit('raw_command', cmd);
+		else if(cmd == "thrust")
+			thrust();
 		e.preventDefault();
 	});
 	document.body.addEventListener("keyup", function(e) {
-		self.keypressed = false;
 		if(!(e.keyCode in self.cmdlist))
 			return;
+		self.cmdlist[e.keyCode].pressed = 0;
 		var cmd = self.cmdlist[e.keyCode].upcmd;
 		if(!cmd)
 			return;
-		self.socket.emit('raw_command', cmd);
+		if(cmd[0] == ';')
+			self.socket.emit('raw_command', cmd);
+		else if(cmd == "thrust")
+			thrust();
 		e.preventDefault();
 	});
 	function init() {
